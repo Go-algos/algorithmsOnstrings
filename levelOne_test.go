c6fceb74c855771da3aff730b845b63721753876
Pavel <pavel.oliynyk@gmail.com>
2025-06-11 16:00:24 +0100

    test: add more tests: currently failing

--- levelOne_test.go
+++ levelOne_test.go
@@ -8,9 +8,15 @@ import (
 
 func TestConstructTrie(t *testing.T) {
 
-	var inputs [][]string = [][]string{{"1", "ATA"}, {"3", "AT", "AG", "AC"}}
+	var inputs [][]string = [][]string{
+		{"1", "ATA"},
+		{"3", "AT", "AG", "AC"},
+		{"3", "ATAGA", "ATC", "GAT"}}
 
-	var outputs [][]string = [][]string{[]string{"0->1:A", "2->3:A", "1->2:T"}, []string{"0->1:A", "1->4:C", "1->3:G", "1->2:T"}}
+	var outputs [][]string = [][]string{
+		{"0->1:A", "2->3:A", "1->2:T"},
+		{"0->1:A", "1->4:C", "1->3:G", "1->2:T"},
+		{"0->1:A", "1->2:T", "2->3:A", "3->4:G", "4->5:A", "2->6:C", "0->7:G", "7->8:A", "8->9:T"}}
 
 	for index := 0; index < len(inputs); index++ {
 		input := inputs[index]

5ac87f6609595d8eff0394275fcbb8ab4bf8f362
Pavel <pavel.oliynyk@gmail.com>
2025-06-11 10:35:03 +0100

    test: add more test coverage

--- levelOne.go
+++ levelOne.go
@@ -2,6 +2,6 @@ package main
 
 // Problem: Construct a Trie from a Collection of Patterns
 func constructTrie(input []string) []string {
-	var output []string = []string{"0->1:A", "2->3:A", "1->2:T" }
+	var output []string = []string{"0->1:A", "2->3:A", "1->2:T"}
 	return output
 }
--- levelOne_test.go
+++ levelOne_test.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 func TestConstructTrie(t *testing.T) {
-	var input []string = []string{"1", "AA"}
-	var output []string = []string{"0->1:A", "2->3:A", "1->2:T"}
-	var result = constructTrie(input)
-	//if result != output {
-	if !reflect.DeepEqual(result, output) {
-		t.Errorf(`constructTrie() = %q, %v`, result, output)
+
+	var inputs [][]string = [][]string{[]string{"1", "AA"}, []string{"3", "AT", "AG", "AC"}}
+
+	var outputs [][]string = [][]string{[]string{"0->1:A", "2->3:A", "1->2:T"}, []string{"0->1:A", "1->4:C", "1->3:G", "1->2:T"}}
+
+	for index := 0; index < len(inputs); index++ {
+		input := inputs[index]
+		output := outputs[index]
+		var result = constructTrie(input)
+		//if result != output {
+		if !reflect.DeepEqual(result, output) {
+			t.Errorf(`constructTrie() = %q, %v`, result, output)
+		}
 	}
 }
